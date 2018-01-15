961577e9edf1c2992a59b3210c467cf8999e1d35
nathan <1121380402@qq.com>
2018-01-15 10:25:16 +0800

    fdasfdsaf

--- nathan/alpha-example/pages/admin/payment.vue
+++ nathan/alpha-example/pages/admin/payment.vue
@@ -3,6 +3,7 @@
     <div class="x_header">
                     <nuxt-link to="/admin/payment">充值</nuxt-link>
                     <nuxt-link to="/admin/payment/withdraw">提现</nuxt-link>
+                    <nuxt-link to="/admin/payment/transfer">转账</nuxt-link>
                     <nuxt-link to="/admin/payment/chargeReport">充值记录</nuxt-link>
                     <nuxt-link to="/admin/payment/withdrawReport">提现记录</nuxt-link>
                     
